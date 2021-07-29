ae3af304dd2c34e85937262d3e5415bf5bb5833c
Omega <omega@omegas-air.lan>
2021-07-29 11:31:42 +0800

    Initial build

---
+++ youtube-onedb/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  youtube-onedb
+//
+//  Created by Omega on 29/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
