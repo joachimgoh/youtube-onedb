fb35630dd2bc9cbd1ef5e024c766e021401e3ccd
Omega <omega@omegas-air.lan>
2021-09-06 23:01:11 +0800

    update TableView plus Const Temp2

--- youtube-onedb/ConstantsTemplate.swift
+++ youtube-onedb/ConstantsTemplate.swift
@@ -14,3 +14,4 @@ struct Constants {
     static var API_URL = ""https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
 }
+
