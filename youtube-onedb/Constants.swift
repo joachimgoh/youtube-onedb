cb0cdeb541ac6e6f18701a6428355a5cb1a6ff58
Omega <omega@omegas-air.lan>
2021-09-06 14:16:36 +0800

    update constants

--- youtube-onedb/Constants.swift
+++ youtube-onedb/Constants.swift
@@ -9,7 +9,7 @@ import Foundation
 
 struct Constants {
     
-    static var API_KEY = "AIzaSyAUaSBQ_4b7EaMM6ssKASHtJ1ldDtxS0ak"
+    static var API_KEY = ""
     static var PLAYLIST_ID = "PLX6gBVSAR-Ih0lGx2Wg1hadwvkb1iTaOu"
     static var API_URL =
         "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
